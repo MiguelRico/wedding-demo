98e4c9d73fbcb3482c648d285111d84a0d234419
Miguel Rico <miguel.rico.vazquez@gmail.com>
2026-07-22 18:21:09 +0200

    Fix 2

--- src/constants/adminContent.js
+++ src/constants/adminContent.js
@@ -138,7 +138,7 @@ const defaultAdminContent = {
       fileName: "boda",
     },
     confirmationsPdf: {
-      eyebrow: "Asistencia",
+      eyebrow: "Asistenciaaaa",
       title: "Invitados del evento",
       text: "Agrupados por confirmación",
       action: "Descargar PDF",
